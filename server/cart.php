c7b56f12745c76c8c13efb3643c7b2e580dbc4cf
钟桉 <2280781762@qq.com>
2020-04-05 22:41:34 +0800

    第三次提交

---
+++ server/cart.php
@@ -0,0 +1,24 @@
+<?php
+header("content-type:text/html;charset=utf8");
+$con = mysqli_connect("localhost","root","root","test");
+mysqli_query($con,"set names utf8");
+$ids = $_POST["ids"];
+$res = mysqli_query($con,"select * from scenics where id in($ids)");
+$arr = [];
+while($row = mysqli_fetch_assoc($res)){
+    $arr[] = $row;
+}
+if(count($arr)>0){
+    $result = [
+        "status"=>200,
+        "msg"=>"ok",
+        "data"=>$arr
+    ];
+}else{
+    $result = [
+        "status"=>404,
+        "msg"=>"data is not found",
+    ];
+}
+echo json_encode($result);
+?>
